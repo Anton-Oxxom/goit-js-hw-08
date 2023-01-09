c8c7025c69d7a5d7265b21998bb49707276dbc4f
Anton-Oxxom <anton.shchetynin@gmail.com>
2023-01-09 15:32:08 +0200

    Update 03-feedback.js

---
+++ src/js/03-feedback.js
@@ -0,0 +1,40 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onformEl, 500));
+
+fillForm();
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    formData.email = formEl.elements.email.value;
+    formData.message = formEl.elements.message.value;
+
+    console.log(formData);
+
+    formEl.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onformEl(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function fillForm() {
+    const savedForm = localStorage.getItem(STORAGE_KEY);
+    if (savedForm) {
+        const parceSavedForm = JSON.parse(savedForm);
+        
+        for (const prop in parceSavedForm) {
+        if (parceSavedForm.hasOwnProperty(prop)) {
+            formEl.elements[prop].value = parceSavedForm[prop];
+            formData[prop] = parceSavedForm[prop];
+        }
+    }
+    }  
+};
